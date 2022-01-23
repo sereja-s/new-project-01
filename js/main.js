5561cf302aaf52b284470fcc574b09cf1bfb409c
sereja-s <serejasuvorov1979@gmail.com>
2022-01-23 20:09:17 +0300

    добавлены пояснения по slick-слайдеру

--- js/main.js
+++ js/main.js
@@ -1,35 +1,39 @@
-$(function(){
+//ПОДКЛЮЧЕНИЕ slick-слайдера (для секции section class="carousel"):
 
-  $('.carousel__inner').slick({
-    arrows: false,
-    dots: true,
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 841,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 601,
-        settings: {
-          slidesToShow: 1
-        }
-      },
-    ]
-  });
+$(function () {
 
-  wow = new WOW(
-    {
-      boxClass: 'wow',      // default
-      animateClass: 'animate__animated', // default
-      offset: 0,          // default
-      mobile: true,       // default
-      live: true        // default
-    }
-  )
-  wow.init();
+	// обратмся к родителю всех внутренних элементов слайдера и подключить функцию slick() из соотвествующей библиотеки:
+	$('.carousel__inner').slick({
+		// укажем все необходимые свойства слайдера:
+		arrows: false,
+		dots: true,
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 841,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 601,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+		]
+	});
+
+	wow = new WOW(
+		{
+			boxClass: 'wow',      // default
+			animateClass: 'animate__animated', // default
+			offset: 0,          // default
+			mobile: true,       // default
+			live: true        // default
+		}
+	)
+	wow.init();
 
 
 });
